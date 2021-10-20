e9cbedea81ce8b593c6c517a657c1f644a799752
mikaelangelo12 <mikaelangelo12@gmail.com>
2021-10-20 08:15:22 -0300

    Chamando as imagens

--- backEnd/src/routes/personagens/index.js
+++ backEnd/src/routes/personagens/index.js
@@ -1,5 +1,11 @@
+const { default: axios } = require("axios")
+
 module.exports = app => {
-    app.get('/personagens',  (req, res) => res.send(' Personagens, Olá Mundo'))
-    app.post('/personagens',  (req, res) =>{
-        res.send('VocÊ esta fazendo um post')})
+      app.get('/api/personagens', async(req, res) => {
+                // data fora do response
+                const {data} = await axios('https://gateway.marvel.com/v1/public/characters?ts=thesoer&apikey=001ac6c73378bbfff488a36141458af2&hash=72e5ed53d1398abb831c3ceec263f18b')
+                return res.json(data)
+        })
+      app.post('/api/personagens', (req, res) =>{
+                res.send('VocÊ esta fazendo um post')})
 }
--- backEnd/src/routes/stories/index.js
+++ backEnd/src/routes/stories/index.js
@@ -1,6 +1,12 @@
+const { default: axios } = require("axios")
+
 module.exports = app => {
-    app.get('/stories',  (req, res) => res.send(' Stories, Olá Mundo'))
-    
-    app.post('/stories',  (req, res) =>{
-        res.send('VocÊ esta fazendo um post')})
+      app.get('/api/stories', async(req, res) => {
+                // data fora do response
+                const {data} = await axios('https://gateway.marvel.com/v1/public/stories?ts=thesoer&apikey=001ac6c73378bbfff488a36141458af2&hash=72e5ed53d1398abb831c3ceec263f18b')
+
+                return res.json(data)
+        })
+      app.post('/api/stories', (req, res) =>{
+                res.send('VocÊ esta fazendo um post')})
 }
